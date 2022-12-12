4f8efaf3eeccaf00b9bc9801019ab8b195ceae82
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-12 22:35:51 +0000

    modified:   my-app/src/App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ import film from './images/film.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="Glasgow Film festival">
+      <header className="TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST">
       <img src={film} alt="film" width="400" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
