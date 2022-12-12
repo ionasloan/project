0b6181fc7f4993c95b8bd8cbff65c9da75993bf5
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-12 22:21:57 +0000

    modified:   my-app/package-lock.json 	modified:   my-app/package.json 	modified:   my-app/src/App.js 	new file:   my-app/src/images/film.jpg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
+import film from './images/film.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Glasgow Film festival">
+      <img src={film} alt="film" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
