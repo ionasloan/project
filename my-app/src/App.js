1875330295c0edd9273d9e7211cf0bb0c31d3c66
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-12 22:37:01 +0000

    modified:   my-app/src/App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,9 @@ import film from './images/film.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST">
-      <img src={film} alt="film" width="400" />
+      <header className="App-header">
+        <h1>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</h1>
+        <img src={film} alt="film" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
