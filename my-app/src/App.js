9711991e6e1d72ed718e68f083ffd1d945e87f2c
ionasloan <66360926+ionasloan@users.noreply.github.com>
2022-12-12 23:08:09 +0000

    Header change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 import film from './images/film.jpg';
@@ -6,26 +6,14 @@ import film from './images/film.jpg';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="Glasgow Film festival - Coming to you March 2023">
+      <header className="Glasgow Film festival">
       <img src={film} alt="film" width="400" />
-=======
-      <header className="App-header">
-        <h1>Glasgow Film Festival</h1>
+        <h1>Glasgow Film Festival  - Coming to you March 2023</h1>
         <img src={film} alt="film" />
->>>>>>> 8195108982a22ea11f42da6161bfd698b789527b
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Come join us 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
       </header>
     </div>
   );
